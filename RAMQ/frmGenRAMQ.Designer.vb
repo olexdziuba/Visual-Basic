4ccd01a811987998be3b25bd36d259f249c5a171
Oleksandr Dziuba <58835413+olexdziuba@users.noreply.github.com>
2024-04-21 17:32:03 -0400

    Add files via upload

---
+++ RAMQ/frmGenRAMQ.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGenRAMQ
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNom = New System.Windows.Forms.TextBox()
+        Me.txtPrenom = New System.Windows.Forms.TextBox()
+        Me.lblNom = New System.Windows.Forms.Label()
+        Me.lblPrenom = New System.Windows.Forms.Label()
+        Me.radHomme = New System.Windows.Forms.RadioButton()
+        Me.radFemme = New System.Windows.Forms.RadioButton()
+        Me.grbSexe = New System.Windows.Forms.GroupBox()
+        Me.updAnee = New System.Windows.Forms.NumericUpDown()
+        Me.updMois = New System.Windows.Forms.NumericUpDown()
+        Me.updJour = New System.Windows.Forms.NumericUpDown()
+        Me.lblAnee = New System.Windows.Forms.Label()
+        Me.lblMois = New System.Windows.Forms.Label()
+        Me.lblJour = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNumeroAssurance = New System.Windows.Forms.TextBox()
+        Me.btnGenere = New System.Windows.Forms.Button()
+        Me.btnQuitter = New System.Windows.Forms.Button()
+        Me.btnEffacer = New System.Windows.Forms.Button()
+        Me.grbSexe.SuspendLayout()
+        CType(Me.updAnee, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.updMois, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.updJour, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtNom
+        '
+        Me.txtNom.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtNom.Location = New System.Drawing.Point(45, 46)
+        Me.txtNom.Name = "txtNom"
+        Me.txtNom.Size = New System.Drawing.Size(130, 20)
+        Me.txtNom.TabIndex = 0
+        '
+        'txtPrenom
+        '
+        Me.txtPrenom.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtPrenom.Location = New System.Drawing.Point(45, 94)
+        Me.txtPrenom.Name = "txtPrenom"
+        Me.txtPrenom.Size = New System.Drawing.Size(130, 20)
+        Me.txtPrenom.TabIndex = 1
+        '
+        'lblNom
+        '
+        Me.lblNom.AutoSize = True
+        Me.lblNom.Location = New System.Drawing.Point(49, 30)
+        Me.lblNom.Name = "lblNom"
+        Me.lblNom.Size = New System.Drawing.Size(29, 13)
+        Me.lblNom.TabIndex = 2
+        Me.lblNom.Text = "Nom"
+        '
+        'lblPrenom
+        '
+        Me.lblPrenom.AutoSize = True
+        Me.lblPrenom.Location = New System.Drawing.Point(49, 78)
+        Me.lblPrenom.Name = "lblPrenom"
+        Me.lblPrenom.Size = New System.Drawing.Size(43, 13)
+        Me.lblPrenom.TabIndex = 3
+        Me.lblPrenom.Text = "Prenom"
+        '
+        'radHomme
+        '
+        Me.radHomme.AutoSize = True
+        Me.radHomme.Checked = True
+        Me.radHomme.Location = New System.Drawing.Point(25, 26)
+        Me.radHomme.Name = "radHomme"
+        Me.radHomme.Size = New System.Drawing.Size(61, 17)
+        Me.radHomme.TabIndex = 4
+        Me.radHomme.TabStop = True
+        Me.radHomme.Text = "Homme"
+        Me.radHomme.UseVisualStyleBackColor = True
+        '
+        'radFemme
+        '
+        Me.radFemme.AutoSize = True
+        Me.radFemme.Location = New System.Drawing.Point(96, 26)
+        Me.radFemme.Name = "radFemme"
+        Me.radFemme.Size = New System.Drawing.Size(59, 17)
+        Me.radFemme.TabIndex = 5
+        Me.radFemme.Text = "Femme"
+        Me.radFemme.UseVisualStyleBackColor = True
+        '
+        'grbSexe
+        '
+        Me.grbSexe.Controls.Add(Me.radFemme)
+        Me.grbSexe.Controls.Add(Me.radHomme)
+        Me.grbSexe.Location = New System.Drawing.Point(21, 130)
+        Me.grbSexe.Name = "grbSexe"
+        Me.grbSexe.Size = New System.Drawing.Size(175, 68)
+        Me.grbSexe.TabIndex = 6
+        Me.grbSexe.TabStop = False
+        Me.grbSexe.Text = "Sexe"
+        '
+        'updAnee
+        '
+        Me.updAnee.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.updAnee.Location = New System.Drawing.Point(117, 217)
+        Me.updAnee.Maximum = New Decimal(New Integer() {2018, 0, 0, 0})
+        Me.updAnee.Minimum = New Decimal(New Integer() {1920, 0, 0, 0})
+        Me.updAnee.Name = "updAnee"
+        Me.updAnee.Size = New System.Drawing.Size(47, 20)
+        Me.updAnee.TabIndex = 7
+        Me.updAnee.Value = New Decimal(New Integer() {1990, 0, 0, 0})
+        '
+        'updMois
+        '
+        Me.updMois.Location = New System.Drawing.Point(117, 252)
+        Me.updMois.Maximum = New Decimal(New Integer() {12, 0, 0, 0})
+        Me.updMois.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.updMois.Name = "updMois"
+        Me.updMois.Size = New System.Drawing.Size(47, 20)
+        Me.updMois.TabIndex = 8
+        Me.updMois.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'updJour
+        '
+        Me.updJour.Location = New System.Drawing.Point(117, 290)
+        Me.updJour.Maximum = New Decimal(New Integer() {31, 0, 0, 0})
+        Me.updJour.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.updJour.Name = "updJour"
+        Me.updJour.Size = New System.Drawing.Size(47, 20)
+        Me.updJour.TabIndex = 9
+        Me.updJour.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'lblAnee
+        '
+        Me.lblAnee.AutoSize = True
+        Me.lblAnee.Location = New System.Drawing.Point(60, 224)
+        Me.lblAnee.Name = "lblAnee"
+        Me.lblAnee.Size = New System.Drawing.Size(38, 13)
+        Me.lblAnee.TabIndex = 10
+        Me.lblAnee.Text = "Année"
+        '
+        'lblMois
+        '
+        Me.lblMois.AutoSize = True
+        Me.lblMois.Location = New System.Drawing.Point(63, 259)
+        Me.lblMois.Name = "lblMois"
+        Me.lblMois.Size = New System.Drawing.Size(29, 13)
+        Me.lblMois.TabIndex = 11
+        Me.lblMois.Text = "Mois"
+        '
+        'lblJour
+        '
+        Me.lblJour.AutoSize = True
+        Me.lblJour.Location = New System.Drawing.Point(65, 297)
+        Me.lblJour.Name = "lblJour"
+        Me.lblJour.Size = New System.Drawing.Size(27, 13)
+        Me.lblJour.TabIndex = 12
+        Me.lblJour.Text = "Jour"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(239, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Votre numéro d'assurance "
+        '
+        'txtNumeroAssurance
+        '
+        Me.txtNumeroAssurance.Location = New System.Drawing.Point(242, 94)
+        Me.txtNumeroAssurance.MaxLength = 12
+        Me.txtNumeroAssurance.Name = "txtNumeroAssurance"
+        Me.txtNumeroAssurance.Size = New System.Drawing.Size(150, 20)
+        Me.txtNumeroAssurance.TabIndex = 14
+        '
+        'btnGenere
+        '
+        Me.btnGenere.Location = New System.Drawing.Point(254, 180)
+        Me.btnGenere.Name = "btnGenere"
+        Me.btnGenere.Size = New System.Drawing.Size(137, 37)
+        Me.btnGenere.TabIndex = 15
+        Me.btnGenere.Text = "Généré"
+        Me.btnGenere.UseVisualStyleBackColor = True
+        '
+        'btnQuitter
+        '
+        Me.btnQuitter.Location = New System.Drawing.Point(363, 309)
+        Me.btnQuitter.Name = "btnQuitter"
+        Me.btnQuitter.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuitter.TabIndex = 16
+        Me.btnQuitter.Text = "Quitter"
+        Me.btnQuitter.UseVisualStyleBackColor = True
+        '
+        'btnEffacer
+        '
+        Me.btnEffacer.Location = New System.Drawing.Point(266, 309)
+        Me.btnEffacer.Name = "btnEffacer"
+        Me.btnEffacer.Size = New System.Drawing.Size(75, 23)
+        Me.btnEffacer.TabIndex = 17
+        Me.btnEffacer.Text = "Effacer"
+        Me.btnEffacer.UseVisualStyleBackColor = True
+        '
+        'frmGenRAMQ
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 344)
+        Me.Controls.Add(Me.btnEffacer)
+        Me.Controls.Add(Me.btnQuitter)
+        Me.Controls.Add(Me.btnGenere)
+        Me.Controls.Add(Me.txtNumeroAssurance)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblJour)
+        Me.Controls.Add(Me.lblMois)
+        Me.Controls.Add(Me.lblAnee)
+        Me.Controls.Add(Me.updJour)
+        Me.Controls.Add(Me.updMois)
+        Me.Controls.Add(Me.updAnee)
+        Me.Controls.Add(Me.grbSexe)
+        Me.Controls.Add(Me.lblPrenom)
+        Me.Controls.Add(Me.lblNom)
+        Me.Controls.Add(Me.txtPrenom)
+        Me.Controls.Add(Me.txtNom)
+        Me.Name = "frmGenRAMQ"
+        Me.Text = "Générateur RAMQ"
+        Me.grbSexe.ResumeLayout(False)
+        Me.grbSexe.PerformLayout()
+        CType(Me.updAnee, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.updMois, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.updJour, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtNom As System.Windows.Forms.TextBox
+    Friend WithEvents txtPrenom As System.Windows.Forms.TextBox
+    Friend WithEvents lblNom As System.Windows.Forms.Label
+    Friend WithEvents lblPrenom As System.Windows.Forms.Label
+    Friend WithEvents radHomme As System.Windows.Forms.RadioButton
+    Friend WithEvents radFemme As System.Windows.Forms.RadioButton
+    Friend WithEvents grbSexe As System.Windows.Forms.GroupBox
+    Friend WithEvents updAnee As System.Windows.Forms.NumericUpDown
+    Friend WithEvents updMois As System.Windows.Forms.NumericUpDown
+    Friend WithEvents updJour As System.Windows.Forms.NumericUpDown
+    Friend WithEvents lblAnee As System.Windows.Forms.Label
+    Friend WithEvents lblMois As System.Windows.Forms.Label
+    Friend WithEvents lblJour As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNumeroAssurance As System.Windows.Forms.TextBox
+    Friend WithEvents btnGenere As System.Windows.Forms.Button
+    Friend WithEvents btnQuitter As System.Windows.Forms.Button
+    Friend WithEvents btnEffacer As System.Windows.Forms.Button
+End Class
---
+++ RAMQ/frmGenRAMQ.vb
@@ -0,0 +1,288 @@
+﻿'Nom du Programme: Generateur RAMQ ver.1.00, Tous droits réservés  © 2018
+' Concepteur : Oleksandr Dziuba gr. 1653
+' Date de creation 20.12.2017
+' Date de fin 16.01.2018
+' Description : Ce programme permet de generer le code RAMQ
+Public Class frmGenRAMQ
+    '@@@@@LES VARIABLES@@@@
+    'les variables pour les informations saisies par l'utilisateur
+    Dim strNom As String 'pour Nom
+    Dim strPrenom As String 'pour prenom
+    Dim strAnee As String 'pour Annee
+    Dim strMois As String 'pour Mois
+    Dim strJour As String 'pour jour
+    ' le variable pour code administrativ 
+    Dim intAdmin As Integer
+    'les variables pour verification de longueur des mots
+    Dim intNomLong As Integer
+    Dim intPrenomLong As Integer
+    'les variables pour verification les symbols dans le Nom et prenoms
+    Dim strVerNom As String 'pour Nom
+    Dim strVerPrenom As String 'pour prenom
+    'les variables pour les lettres acceptables avec lettres acceptables 
+    Dim strlistOfLetters As String() = New String() {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-", "É", "È", "Ù", "Ô", " "}
+    ' variable pour eviter generation le code si Nom ou Prenom ou date n'est pas valide
+    Dim intX As Integer
+    'variable pour calculer Bissextile 
+    Dim intAnee As Integer
+    Dim intBis As Integer
+
+
+    Private Sub btnGenere_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGenere.Click
+
+        '!!!######Generation le code RAMQ####!!!
+
+        ' ######Saisir les donees######
+        intNomLong = Len(txtNom.Text) ' saisir longueur de Nom
+        intPrenomLong = Len(txtPrenom.Text) ' saisir longueur de Prenom
+        strNom = Mid(txtNom.Text, 1, 3) 'saisir 1-3 symbols de Nom
+        strPrenom = Mid(txtPrenom.Text, 1, 1) ' saisir 1 simbol de Prenom
+        strAnee = updAnee.Value 'saisir Annee
+        strMois = updMois.Value 'saisir Mois
+        strJour = updJour.Value 'saisir Jour
+        intX = 1
+
+        '######Verification des donees######
+
+
+        ' --verification  Nom--  
+        'verification minimum 3 symboles
+        If intNomLong < 3 Then
+            MsgBox("Vous devez écrire votre nom au complet, Votre nom doit avoir minimum 3 symboles", vbExclamation + vbOKOnly, "Error")
+            intX = 2
+            'verification si nom a seulement symbols numerique
+        ElseIf IsNumeric(strNom) Then
+            MsgBox("Votre nom est n'est pas valide. Nom ne doit pas avoir symbol nymerique", vbExclamation + vbOKOnly, "Error")
+            intX = 2
+        End If
+
+        ''verifier si il y a les chifres en Nom 
+        If (txtNom.Text <> "") Then
+            strVerNom = txtNom.Text
+            For Each letter In strlistOfLetters
+                strVerNom = Replace(strVerNom, letter, "")
+            Next
+            ' parcourir chaque lettre du  'listOfLetters' et remplacer cette lettre par rien Si les symbols restent - ce sont symbols qui ne sont pas accepteble
+
+            If (strVerNom <> "") Then
+                MsgBox("Votre nom n'est pas valide. Votre nom  doit  avoir seulement les symboles alphabétiques!", vbExclamation + vbOKOnly, "Error")
+                intX = 2
+            End If
+
+        End If
+
+        ' --verification   Prenom --
+        'verification minimum 3 symboles
+        If intPrenomLong < 3 Then
+            MsgBox("Vous devez écrire votre prénom au complet, Votre prénom doit avoir minimum 3 symboles", vbExclamation + vbOKOnly, "Error")
+            intX = 2
+            'verification si prenom a seulement symbols numerique
+        ElseIf IsNumeric(strPrenom) Then
+            MsgBox("Votre prénom  n'est pas valide. Prénom ne doit pas avoir symbol nymerique", vbExclamation + vbOKOnly, "Error")
+            intX = 2
+        End If
+
+        ''verifier si il y a les chifres dans le prenom
+        If (txtPrenom.Text <> "") Then
+
+            strVerPrenom = txtPrenom.Text
+
+
+
+            For Each letter In strlistOfLetters
+                strVerPrenom = Replace(strVerPrenom, letter, "")
+                ' parcourir chaque lettre du  'listOfLetters' et remplacer cette lettre par rien Si les symbols restent - ce sont symbols qui ne sont pas accepteble
+            Next
+
+
+            If (strVerPrenom <> "") Then
+                MsgBox("Votre prénom est n'est pas valide. Votre prénom  doit  avoir seulement les symboles alphabétiques!", vbExclamation + vbOKOnly, "Error")
+                intX = 2
+            End If
+
+        End If
+
+
+
+
+        '--verification anee--
+        If strAnee = 1919 Then
+            MsgBox("Votre Anee de naissanse  n'est pas valide. ", vbExclamation + vbOKOnly, "Error")
+            intX = 2
+        End If
+        ''verification si une année donnée est bissextile ou non
+        intAnee = strAnee
+        If (intAnee Mod 4 = 0) And (intAnee Mod 100 <> 0) Or (intAnee Mod 400 = 0) Then
+            intBis = 1 ' annee est bissextile
+        Else
+
+            intBis = 2 ' annee n'est pas bissextile
+        End If
+        '--verification quantite jour en février --
+        If intBis = 1 And CInt(strMois) = 2 And CInt(strJour) > 29 Then
+            intX = 2
+            MsgBox("Votre Jour de naissanse  n'est pas valide. Le mois février doit avoir maximun 29  jours", vbExclamation + vbOKOnly, "Error")
+        End If
+        If intBis = 2 And CInt(strMois) = 2 And CInt(strJour) > 28 Then
+
+            intX = 2
+            MsgBox("Votre Jour de naissanse  n'est pas valide. Le mois février doit avoir maximun 28  jours", vbExclamation + vbOKOnly, "Error")
+
+        End If
+
+        ' verification janvier, Mars, Mai, Juillet, Aout, Octobre, Décembre
+        Select Case strMois
+            Case 1, 3, 5, 7, 8, 10, 12
+                If CInt(strJour) > 31 Then
+                    MsgBox("Votre Jour de naissanse  n'est pas valide. Les mois janvier, Mars, Mai, Juillet, Aout, Octobre, Décembre doivent avoir maximum 31 jours", vbExclamation + vbOKOnly, "Error")
+                    intX = 2
+                End If
+
+                ' verification Avril, Juin, Septembre, Novembre
+            Case 4, 6, 9, 11
+                If CInt(strJour) > 30 Then
+                    intX = 2
+                    MsgBox("Votre Jour de naissanse  n'est pas valide. Les mois Avril, Juin, Septembre, Novembre doivent avoir maximum 30 jours ", vbExclamation + vbOKOnly, "Error")
+                End If
+                ' verification février
+            Case 2
+                If CInt(strJour) > 29 Then
+                    intX = 2
+                    MsgBox("Votre Jour de naissanse  n'est pas valide. LLe mois février doit avoir 28  ou  29 jours ", vbExclamation + vbOKOnly, "Error")
+                End If
+
+        End Select
+
+
+
+
+
+        '' -calculation mois pour femme-
+        If radFemme.Checked = True Then
+            strMois = strMois + 50
+        End If
+        ''-calculation anee-
+        If strAnee < 2000 Then
+            strAnee = strAnee - 1900
+        Else
+            strAnee = strAnee - 2000
+
+        End If
+
+
+
+
+
+        ' -generation code administrativ-
+        intAdmin = Int(Rnd() * 99) + 1
+        'si code a 1 chiffre on ajoute 10 pour avoir 2
+        If intAdmin < 10 Then
+            intAdmin = intAdmin + 10
+        End If
+        '-Affichage RAMQ-
+        'si mois ou jour est <10, on ajout 10
+
+        If intX = 1 Then
+            If strJour < 10 And strMois < 10 And strAnee < 10 Then
+                txtNumeroAssurance.Text = strNom & strPrenom & "0" & strAnee & "0" & strMois & "0" & strJour & intAdmin
+                txtNom.Text = ""
+                txtPrenom.Text = ""
+                txtPrenom.Text = ""
+                updAnee.Value = 1990
+                updJour.Value = 1
+                updMois.Value = 1
+            ElseIf strJour < 10 And strMois < 10 Then
+                txtNumeroAssurance.Text = strNom & strPrenom & strAnee & "0" & strMois & "0" & strJour & intAdmin
+                txtNom.Text = ""
+                txtPrenom.Text = ""
+                txtPrenom.Text = ""
+                updAnee.Value = 1990
+                updJour.Value = 1
+                updMois.Value = 1
+            ElseIf strJour < 10 And strAnee < 10 Then
+                txtNumeroAssurance.Text = strNom & strPrenom & "0" & strAnee & strMois & "0" & strJour & intAdmin
+                txtNom.Text = ""
+                txtPrenom.Text = ""
+                txtPrenom.Text = ""
+                updAnee.Value = 1990
+                updJour.Value = 1
+                updMois.Value = 1
+            ElseIf strMois < 10 And strAnee < 10 Then
+                txtNumeroAssurance.Text = strNom & strPrenom & "0" & strAnee & "0" & strMois & strJour & intAdmin
+                txtNom.Text = ""
+                txtPrenom.Text = ""
+                txtPrenom.Text = ""
+                updAnee.Value = 1990
+                updJour.Value = 1
+                updMois.Value = 1
+            ElseIf strJour < 10 Then
+                txtNumeroAssurance.Text = strNom & strPrenom & strAnee & strMois & "0" & strJour & intAdmin
+                txtNom.Text = ""
+                txtPrenom.Text = ""
+                txtPrenom.Text = ""
+                updAnee.Value = 1990
+                updJour.Value = 1
+                updMois.Value = 1
+            ElseIf strMois < 10 Then
+                txtNumeroAssurance.Text = strNom & strPrenom & strAnee & "0" & strMois & strJour & intAdmin
+                txtNom.Text = ""
+                txtPrenom.Text = ""
+                txtPrenom.Text = ""
+                updAnee.Value = 1990
+                updJour.Value = 1
+                updMois.Value = 1
+            ElseIf strAnee < 10 Then
+                txtNumeroAssurance.Text = strNom & strPrenom & "0" & strAnee & strMois & strJour & intAdmin
+                txtNom.Text = ""
+                txtPrenom.Text = ""
+                txtPrenom.Text = ""
+                updAnee.Value = 1990
+                updJour.Value = 1
+                updMois.Value = 1
+            Else
+                txtNumeroAssurance.Text = strNom & strPrenom & strAnee & strMois & strJour & intAdmin
+                txtNom.Text = ""
+                txtPrenom.Text = ""
+                txtPrenom.Text = ""
+                updAnee.Value = 1990
+                updJour.Value = 1
+                updMois.Value = 1
+            End If
+        Else
+            ' effacer les donnees 
+            txtNom.Text = ""
+            txtPrenom.Text = ""
+            txtPrenom.Text = ""
+
+            updAnee.Value = 1990
+            updJour.Value = 1
+            updMois.Value = 1
+
+
+        End If
+
+
+
+
+    End Sub
+
+
+
+    Private Sub btnQuitter_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuitter.Click
+        ' fermer la programme 
+        End
+    End Sub
+
+
+
+    Private Sub btnEffacer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEffacer.Click
+        ' effacer les donnees 
+        txtNom.Text = ""
+        txtPrenom.Text = ""
+        txtPrenom.Text = ""
+        txtNumeroAssurance.Text = ""
+        updAnee.Value = 1990
+        updJour.Value = 1
+        updMois.Value = 1
+    End Sub
+End Class
